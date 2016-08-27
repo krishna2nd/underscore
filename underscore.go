a93421b372ced192404381a282e4f09ec4a7a503
Krishna Kumar <krishnass@saltside.se>
2016-08-27 12:20:19 +0530

    Each method with iteratee on array, slice of anytype.

--- underscore.go
+++ underscore.go
@@ -1,1 +1,20 @@
-// implementations pending
+package underscore;
+
+import(
+	"reflect"
+)
+
+type underscore struct {
+}
+
+
+type iteratee func(v interface{})
+func Each(array interface{}, iteratee iteratee) {
+	switch reflect.TypeOf(array).Kind() {
+		case reflect.Slice:
+		    arr := reflect.ValueOf(array)
+		    for i := 0; i < arr.Len(); i++ {
+		        iteratee(arr.Index(i))
+		    }
+    }
+}
